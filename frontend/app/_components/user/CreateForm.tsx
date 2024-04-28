cb3f78c2b76abe9bbe113edf9aadef32277ba814
RyuichiUeda <hsatlefp@gmail.com>
2024-04-28 20:45:26 +0900

    非同期エラーキャッチ

--- frontend/app/_components/user/CreateForm.tsx
+++ frontend/app/_components/user/CreateForm.tsx
@@ -1,41 +1,49 @@
-import React from "react"
-import { createWrapper, createUser } from "@/_services/userService"
-import type { NewUser, CreateFormProps, ExistingUser } from "@/definitions"
-import { Button, TextInput, Flex } from "@mantine/core"
-import { UserForm } from "@/_utils/UserForm"
-import { T } from "@/_intl/T"
-import { useNotification } from "@/_utils/_hooks/notifications"
+import React from "react";
+import { createWrapper, createUser } from "@/_services/userService";
+import type {
+  NewUser,
+  ExistingUserListProps,
+  ExistingUser,
+} from "@/definitions";
+import { Button, TextInput, Flex } from "@mantine/core";
+import { UserForm } from "@/_utils/UserForm";
+import { T } from "@/_intl/T";
+import { useNotification } from "@/_utils/_hooks/notifications";
 
-export const CreateForm: React.FC<CreateFormProps<ExistingUser>> = ({
+export const CreateForm: React.FC<ExistingUserListProps> = ({
   items,
   setItems,
 }) => {
-  const { notifySuccess, notifyError } = useNotification()
+  const { notifySuccess, notifyError } = useNotification();
 
   const form = UserForm({
     name: "",
     email: "",
-  })
+  });
 
   const handleSubmit = async (values: NewUser) => {
     try {
-      createWrapper<ExistingUser, NewUser>({
+      await createWrapper<ExistingUser, NewUser>({
         values,
         items,
         setItems,
         createData: createUser,
-      })
-      notifySuccess()
-      form.reset()
+      });
+      notifySuccess();
+      form.reset();
     } catch (error) {
-      console.error("Error creating user:", error)
-      notifyError()
+      console.error("Error creating user:", error);
+      notifyError();
     }
-  }
+  };
 
   return (
     <form onSubmit={form.onSubmit(handleSubmit)}>
-      <TextInput {...form.getInputProps("name")} label={<T id="Username"/>} withAsterisk />
+      <TextInput
+        {...form.getInputProps("name")}
+        label={<T id="Username" />}
+        withAsterisk
+      />
       <TextInput
         {...form.getInputProps("email")}
         label={<T id="Email" />}
@@ -48,5 +56,5 @@ export const CreateForm: React.FC<CreateFormProps<ExistingUser>> = ({
         </Button>
       </Flex>
     </form>
-  )
-}
+  );
+};
